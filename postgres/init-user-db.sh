fb6920a455d2bef8c379dadb569dab5b444efd13
kaveh <kaveh@illinois.edu>
2020-11-13 11:51:36 -0600

    Add default value for datawolf log variable

--- postgres/init-user-db.sh
+++ postgres/init-user-db.sh
@@ -3,6 +3,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE keycloak;
     CREATE DATABASE datawolf;
     CREATE DATABASE harvest;
     \c harvest
